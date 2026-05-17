48496060bc4f65964548f70313ae7cdf370d7f1d
Kartavya05d <111231860+Kartavya05d@users.noreply.github.com>
2026-05-17 15:56:40 +0000

    improved views.py

--- app/api/views.py
+++ app/api/views.py
@@ -9,4 +9,8 @@ templates = Jinja2Templates(directory=Path(__file__).resolve().parent.parent / "
 
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
